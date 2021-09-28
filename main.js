05590907b1a0458bb8dc5e5a349859a498ef558b
John Do <jdo.dev45@gmail.com>
2021-09-28 09:13:24 -0700

    modified form, used functions

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 let num;
-let userInput = document.getElementById('userInput')
+const userInput = document.getElementById('userInput')
 const userForm = document.getElementById('userForm')
 const prevGuess = document.getElementById('prevGuess')
 
 window.addEventListener('DOMContentLoaded', event => {
   num = Math.floor(Math.random() * 100);
-  console.log(num);
 });
 
-userForm.addEventListener('submit', event => {
+userForm.addEventListener('submit', userSubmit)
+
+function userSubmit(event) {
   event.preventDefault()
   let userNum = userInput.value
 
   if (isNaN(userNum)) return
+
   prevGuess.textContent = prevGuess.textContent + ' ' + userNum
-  userNum = ''
-})
+  userForm.reset()
+}
