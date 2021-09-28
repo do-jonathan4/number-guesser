724d8dfe6090335e2d0a3cb7446939bfbb4d9975
John Do <jdo.dev45@gmail.com>
2021-09-28 08:54:03 -0700

    added styling, user can enter number

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 let num;
+let userInput = document.getElementById('userInput')
+const userForm = document.getElementById('userForm')
+const prevGuess = document.getElementById('prevGuess')
 
 window.addEventListener('DOMContentLoaded', event => {
   num = Math.floor(Math.random() * 100);
   console.log(num);
 });
+
+userForm.addEventListener('submit', event => {
+  event.preventDefault()
+  let userNum = userInput.value
+
+  if (isNaN(userNum)) return
+  prevGuess.textContent = prevGuess.textContent + ' ' + userNum
+  userNum = ''
+})
