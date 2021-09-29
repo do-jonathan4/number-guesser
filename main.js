94c71069ea299fe65ee716b8819c2e71967bafb0
John Do <jdo.dev45@gmail.com>
2021-09-29 08:22:17 -0700

    more styling, user can lose

--- main.js
+++ main.js
@@ -4,26 +4,33 @@ const userInput = document.getElementById('userInput')
 const userForm = document.getElementById('userForm')
 const prevGuess = document.getElementById('prevGuess')
 const userHint = document.getElementById('userHint')
+const endGame = document.getElementById('endGame')
+const userLose = document.getElementById('userLose')
+const userWin = document.getElementById('userWin')
+const restartGame = document.getElementById('restartGame')
 
-window.addEventListener('DOMContentLoaded', event => {
-  num = Math.floor(Math.random() * 100);
-});
-
+window.addEventListener('DOMContentLoaded', genNum);
 userForm.addEventListener('submit', userSubmit)
+
+function genNum() {
+  num = Math.floor(Math.random() * 100);
+}
 
 function userSubmit(event) {
   event.preventDefault()
   let userNum = userInput.value
-  if (isNaN(userNum)) return
+  if (isNaN(userNum) || userNum === '') {
+    userForm.reset()
+    return
+  }
 
   prevGuess.style.display = 'block'
   userHint.style.display = 'block'
   prevGuess.textContent = prevGuess.textContent + ' ' + userNum
-
   userClue(num, userNum)
 
   attempts--
-  if (attempts === 0) userInput.disabled = true
+  if (attempts === 0) gameLoss()
   userForm.reset()
 }
 
@@ -33,6 +40,12 @@ function userClue(num, userNum) {
   } else if (userNum > num) {
     userHint.textContent = 'Guess is too high'
   } else {
-    userHint.textContent = 'Congratulations you win!'
+    userHint.style.display = 'none'
   }
+}
+
+function gameLoss() {
+  userInput.disabled = true
+  endGame.style.display = 'block'
+  userWin.style.display = 'none'
 }
